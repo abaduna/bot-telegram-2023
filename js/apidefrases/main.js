3a7a78730f2e81f5b814f82dedb08fcbabf6ac82
abaduna <64230830+abaduna@users.noreply.github.com>
2023-10-15 21:04:23 -0300

    temrinado el index

--- js/apidefrases/main.js
+++ js/apidefrases/main.js
@@ -7,11 +7,11 @@ const  llamada_A_la_API=async()=>{
 }
 const escrivirData = (data)=>{
     divRes = document.querySelector("#resultado")
-    divRes.innerHTML = ` <div class="card" style="width: 18rem;">
+    divRes.innerHTML = ` <div class="card " style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${data.value}</h5>
       <p class="card-text"> fue creada el ${data.created_at}</p>
-      
+      <button class="btn  boton" onclick="llamada_A_la_API()" type="submit">Buscar otra</button>
     </div>
   </div>`
 
